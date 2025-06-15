34ba9fcced4b28695a501e7e289b56f57e57ecda
Oth3r <68134921+Oth3r@users.noreply.github.com>
2025-06-14 19:55:41 -0500

    port OtterHelper

---
+++ paper/src/main/java/one/oth3r/otterlib/OtterHelper.java
@@ -0,0 +1,35 @@
+package one.oth3r.otterlib;
+
+import net.kyori.adventure.text.TextComponent;
+import one.oth3r.otterlib.base.LoaderUtilities;
+import one.oth3r.otterlib.chat.CTxT;
+
+import java.util.logging.Logger;
+
+public class OtterHelper implements LoaderUtilities {
+    /**
+     * gets the logger for the current OtterLib instance
+     */
+    @Override
+    public Logger getLogger() {
+        return Logger.getLogger("OtterLib");
+    }
+
+    /**
+     * Trys to convert the object into a CTxT. <br/>
+     * on different loaders, different tactics are used to grab the loader's different default Text implementations
+     *
+     * @return the CTxT
+     */
+    @Override
+    public CTxT getCTxTFromObj(Object obj) {
+        CTxT output = new CTxT();
+        // append the correctly cast object to the output
+        if (obj instanceof CTxT) output.append(((CTxT) obj).b());
+        else if (obj instanceof TextComponent) output.append((TextComponent) obj);
+        // else, try to convert into a string
+        else output.append(String.valueOf(obj));
+
+        return output;
+    }
+}
