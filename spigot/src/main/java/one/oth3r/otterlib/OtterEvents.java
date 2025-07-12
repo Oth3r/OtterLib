b718bb4f97122f7f074d96e56b9f70daa3277d43
Oth3r <68134921+oth3r@users.noreply.github.com>
2025-07-11 19:01:33 -0500

    update file reg name

--- spigot/src/main/java/one/oth3r/otterlib/OtterEvents.java
+++ spigot/src/main/java/one/oth3r/otterlib/OtterEvents.java
@@ -1,6 +1,6 @@
 package one.oth3r.otterlib;
 
-import one.oth3r.otterlib.file.FileRegistry;
+import one.oth3r.otterlib.registry.CustomFileReg;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.server.ServerLoadEvent;
@@ -9,7 +9,7 @@ public class OtterEvents implements Listener {
     @EventHandler
     public static void serverLoadEvent(ServerLoadEvent event) {
         // this is called when the server is loaded, so we can queue file loading here
-        FileRegistry.queueLoading();
+        CustomFileReg.queueLoading();
 
         // finish initialization
         OtterLib.isInitialized = true;
