7fb88599c12d77d737104edc4498eeebe01ea904
Oth3r <68134921+oth3r@users.noreply.github.com>
2025-07-18 22:58:11 -0500

    click action docs and static factory

--- common/src/main/java/one/oth3r/otterlib/chat/click/ClickAction.java
+++ common/src/main/java/one/oth3r/otterlib/chat/click/ClickAction.java
@@ -6,27 +6,61 @@ public class ClickAction {
     protected final ClickActions action;
     protected final String actionString;
 
+    /**
+     * constructor for action by enum
+     * @param action action enum
+     * @param actionString string for the action to take
+     */
     public ClickAction(ClickActions action, String actionString) {
         this.action = action;
         this.actionString = actionString;
     }
 
+    /**
+     * constructor for action by code
+     * @param action action code
+     * @param actionString string for the action to take
+     */
     public ClickAction(int action, String actionString) {
         this.action = Enums.search((e) -> e.getCode() == action, ClickActions.class).stream()
                 .findFirst().orElse(ClickActions.NOTHING);
         this.actionString = actionString;
     }
 
+    /**
+     * copy constructor
+     * @param action action to copy
+     */
     public ClickAction(ClickAction action) {
         this.action = action.action;
         this.actionString = action.actionString;
     }
 
+    /**
+     * @return the action enum
+     */
     public ClickActions getAction() {
         return action;
     }
 
+    /**
+     * @return the action string
+     */
     public String getActionString() {
         return actionString;
+    }
+
+    /**
+     * static factory method to create a ClickAction with an enum
+     */
+    public static ClickAction of(ClickActions action, String actionString) {
+        return new ClickAction(action, actionString);
+    }
+
+    /**
+     * static factory method to create a ClickAction with an action code
+     */
+    public static ClickAction of(int action, String actionString) {
+        return new ClickAction(action, actionString);
     }
 }
