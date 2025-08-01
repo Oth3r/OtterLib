b201ad57e53861014a66ea3a3394f8659a4d5ae6
Oth3r <68134921+oth3r@users.noreply.github.com>
2025-08-01 18:59:51 -0500

    add more CTxT() examples

--- fabric-test/src/main/java/one/oth3r/fabricTest/FabricTest.java
+++ fabric-test/src/main/java/one/oth3r/fabricTest/FabricTest.java
@@ -4,6 +4,8 @@ import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.networking.v1.ServerPlayConnectionEvents;
 import one.oth3r.otterlib.chat.CTxT;
 import one.oth3r.otterlib.chat.Rainbow;
+import one.oth3r.otterlib.chat.Wrapper;
+import one.oth3r.otterlib.chat.hover.HoverAction;
 import one.oth3r.otterlib.registry.CustomFileReg;
 
 import java.awt.*;
@@ -16,8 +18,12 @@ public class FabricTest implements ModInitializer {
         CustomFileReg.registerFile(MOD_ID, new CustomFileReg.FileEntry(TestFile.ID, new TestFile(), true, true));
 
         ServerPlayConnectionEvents.JOIN.register((handler, sender, server) -> {
-            handler.player.sendMessage(new CTxT("Hello").color(Color.BLUE).bold(true).strikethrough(true)
-                    .append(new CTxT("World!!!!!!!!!").rainbow(new Rainbow(true)).underline(true).italic(true)).b());
+            handler.player.sendMessage(new CTxT()
+                    .append(new CTxT("Hello").color(Color.BLUE).bold(true).strikethrough(true).wrapper())
+                    .append(new CTxT("World!!!!!!!!!").rainbow(new Rainbow(true)).underline(true).italic(true)
+                            .hover(HoverAction.of(new CTxT("man this is hovering rn").bold())))
+                    .append(new CTxT("button").hover(HoverAction.of(new CTxT("hovering it up"))).color(Color.BLUE).wrapper(new Wrapper<>(new CTxT("{").color(Color.YELLOW).bold(), new CTxT("}").italic())))
+                    .b());
             });
     }
 
